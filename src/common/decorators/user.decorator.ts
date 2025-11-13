ddcdac26f0fce174493b13c0c2beb3fd65e210ed
Slade <saib6064@gmail.com>
2025-11-13 15:54:10 +0700

    feat: create custom user decorator to extract user from request

---
+++ src/common/decorators/user.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
+
+export const User = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+
+    const user = request['user'];
+
+    return data ? user?.[data] : user;
+  },
+);
